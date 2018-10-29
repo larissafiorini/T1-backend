b1096ec59c54069765209285e852f6ed8493750d
mathiasVoelckerPUCRS <mathias.voelcker@acad.pucrs.br>
2018-10-29 19:30:53 -0300

    finalizar CRUD básico em baseService

--- app.js
+++ app.js
@@ -12,16 +12,24 @@ var atributosValores = [];
 atributos.push(new Column('rua', TypeEnum.string, null, false));
 atributos.push(new Column('numero', TypeEnum.int, null, true));
 
-atributosValores.push(new Atribute(name = 'rua', value = 'victor kessler'));
-atributosValores.push(new Atribute(name = 'numero', value = 31));
+atributosValores.push(new Atribute(name = 'rua', value = 'plinio'));
+atributosValores.push(new Atribute(name = 'numero', value = 201));
 
-//service.createTable('endereco', atributos);
-//service.insertInto('endereco', atributosValores);
-var allFields = service.findAll('Eu');
-console.log(allFields);
+// service.createTable('endereco', atributos);
+// service.insertInto('endereco', atributosValores);
+// var teste
+// service.findAll('Eu').then((value) => { 
+//     teste = value
+//     console.log(teste);
+// });
+// service.update('Eu', new Atribute(name = 'idade', value = 24), 3).then((value) => { 
+//     teste = value
+//     console.log(teste);
+// });
+service.delete('Eu', 2);
 
-//service.deleteRow('endereco','id','2');
 
-//service.updateRow('Eu','id','1','nome','novonome');
+// service.findById('Eu', 3)
+
 // console.log(teste);
 // teste
--- service/baseService.js
+++ service/baseService.js
@@ -31,34 +31,82 @@ module.exports = {
     },
 
     insertInto(tableName, atributes) { 
-        let connection = mysql.createConnection(config);
+        return new Promise(
+            (resolve, reject) => {
+            let connection = mysql.createConnection(config);
+            
+            sql = `insert into ${tableName} (`
+            for(var i = 0; i < atributes.length; i++) {
+                sql += `${atributes[i].name},`
+            }
+            sql = sql.substring(0, sql.length-1);
+            sql += ') VALUES (';
+            for(var i = 0; i < atributes.length; i++) {            
+                sql += `'${atributes[i].value}',`
+            }
+            sql = sql.substring(0, sql.length-1);
+            sql += ')';
+            connection.query(sql, function(err, results, fields) {
+                if (err) {
+                    reject(err.message);
+                } else {
+                    resolve(results);
+                }
+            });
         
-        sql = `insert into ${tableName} (`
-        for(var i = 0; i < atributes.length; i++) {
-            sql += `${atributes[i].name},`
-        }
-        sql = sql.substring(0, sql.length-1);
-        sql += ') VALUES (';
-        for(var i = 0; i < atributes.length; i++) {            
-            sql += `'${atributes[i].value}',`
-        }
-        sql = sql.substring(0, sql.length-1);
-        sql += ')';
-        connection.query(sql, function(err, results, fields) {
-            if (err) {
-                console.log(err.message);
-            }
-        });
-    
-        connection.end(function(err) {
-            if (err) {
-                return console.log(err.message);
-            }
-        }); 
+            connection.end(function(err) {
+                if (err) {
+                    return console.log(err.message);
+                }
+            });
+        }) 
     },
 
     findAll(tableName) {
+        return new Promise(
+            (resolve, reject) => {
+                let connection = mysql.createConnection(config);
+                sql = `select * from ${tableName}`;
+                return connection.query(sql, function(err, results, fields) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(results); 
+                    }
+                    connection.end(function(err) {
+                        if (err) {
+                            return console.log(err.message);
+                        }
+                    }); 
+                });
+            })
+    },
+
+    findById(tableName, id) {
+        return new Promise(
+            (resolve, reject) => {
+                let connection = mysql.createConnection(config);
+                sql = `select * from ${tableName} where id = ${id}`;
+                return connection.query(sql, function(err, results, fields) {
+                    if (err) {
+                        reject(err.message);
+                    } else {
+                        resolve(results); 
+                    }
+                    connection.end(function(err) {
+                        if (err) {
+                            return console.log(err.message);
+                        }
+                    }); 
+                });
+            })
+    },
+
+    update(tableName, atribute, id) {
+        return new Promise(
+            (resolve, reject) => {
             let connection = mysql.createConnection(config);
+<<<<<<< HEAD
             sql = `select * from ${tableName}`;
             connection.query(sql, function(err, results, fields) {
                 if (err) {
@@ -112,6 +160,44 @@ module.exports = {
                 return console.log(err.message);
             }
         }); 
+=======
+            sql = `update ${tableName} set ${atribute.name} = ${atribute.value} where id = ${id}`
+            connection.query(sql, function(err, results, fields) {
+                if (err) {
+                    reject(err.message);
+                }
+                else {
+                    resolve(results)
+                }
+            });
+            connection.end(function(err) {
+                if (err) {
+                    return console.log(err.message);
+                }
+            });
+        })
+    },
+
+    delete(tableName, id) {
+        return new Promise(
+            (resolve, reject) => {
+            let connection = mysql.createConnection(config);
+            sql = `delete from ${tableName} where id = ${id}`
+            connection.query(sql, function(err, results, fields) {
+                if (err) {
+                    reject(err.message);
+                }
+                else {
+                    resolve(results)
+                }
+            });
+            connection.end(function(err) {
+                if (err) {
+                    return console.log(err.message);
+                }
+            });
+        })
+>>>>>>> finalizar CRUD básico em baseService
     }
 
     
